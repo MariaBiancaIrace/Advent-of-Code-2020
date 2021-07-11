44c209f1f1e76e77c314ca4cddf98c98e56d017b
Maria Bianca Irace <mariabiancairace@gmail.com>
2021-07-11 17:13:48 -0300

    Adiciona código da parte 1 do dia 4 e renomeia outras partes 1.

---
+++ 2020-04-code1.py
@@ -0,0 +1,16 @@
+with open("2020-04-input.txt") as arquivo:
+    lista_linhas = arquivo.read()
+
+lista_linhas = lista_linhas.replace('\n\n','AAAAA')
+lista_linhas = lista_linhas.replace('\n',' ')
+lista_linhas = lista_linhas.replace('AAAAA','\n').splitlines()
+
+contador_validos = 0
+substring = 'byr'
+
+for linha in lista_linhas:
+    if ('byr' in linha and 'iyr' in linha and 'eyr' in linha and 'hgt' in linha and 'hcl' in linha and 'ecl' in linha and 'pid' in linha):
+        contador_validos += 1
+        print(linha, contador_validos)
+
+
