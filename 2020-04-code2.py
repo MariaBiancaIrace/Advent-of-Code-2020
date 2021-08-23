5ae66a2a2c1ed8eafa95144be5d596d684a8e382
Maria Bianca Irace <mariabiancairace@gmail.com>
2021-08-23 01:33:18 -0300

    Finaliza resolução da parte 2 do dia 4.

--- 2020-04-code2.py
+++ 2020-04-code2.py
@@ -17,6 +17,7 @@ for linha in lista_linhas:
         lista_itens = linha.strip().split(' ')
         dicionario = {}
         lista_itens_validar = ['byr', 'eyr', 'iyr', 'hgt', 'hcl', 'ecl', 'pid']
+        lista_cores_validas = ['amb','blu','brn','gry','grn','hzl','oth']
         for itens in lista_itens:
             chave,valor = itens.strip().split(':')
             dicionario[chave] = valor
@@ -26,7 +27,14 @@ for linha in lista_linhas:
             if (int(dicionario['byr']) in range(1920,2003)) and (int(dicionario['iyr']) in range(2010,2021)) and (int(dicionario['eyr']) in range(2020,2031)):
                 if (('in' in dicionario['hgt']) and (int(dicionario['hgt'][:-2]) in range(59,77))) or (('cm' in dicionario['hgt']) and (int(dicionario['hgt'][:-2]) in range(150,194))):
                     if(('#' in dicionario['hcl']) and all(caracteres in string.hexdigits for caracteres in dicionario['hcl'][1:])):
-                        print(dicionario, 'OK',dicionario['hcl'][1:],len(dicionario['hcl'][1:]))
+                        if dicionario['ecl'] in lista_cores_validas:
+                            if len(dicionario['pid']) == 9:
+                                contador_validos += 1
+                                print(dicionario, 'OK',dicionario['pid'],contador_validos)
+                            else:
+                                print(dicionario,'pid zoado')
+                        else:
+                            print(dicionario,'ecl zoada')
                     else:
                         print(dicionario,'hcl zoado')
                 else:
@@ -35,26 +43,3 @@ for linha in lista_linhas:
                 print(dicionario,'valor ruim byr,iyr ou eyr')
         else:
             print(dicionario,'NOK')
-            #total_chave_valor = len(itens.strip().split(':'))
-            #print(itens, total_itens,total_chave_valor)
-    ##linha = linha.split(' ')
-    ##dicionario = {}
-    ##for item in linha:
-        ##print(len(item), len(item.strip().split(':')))
-        ##chave,valor = item.strip().split(':')
-        ##if valor is None:
-        ##    valor='0'    
-        ##dicionario[chave] = valor
-    ##print(dicionario,type(dicionario))
-    
-    #if ('byr','iyr','eyr') in dicionario.keys(): 
-    #    if int(dicionario['byr']) in range(1920,2003) and int(dicionario['iyr']) in range(2010,2021) and int(dicionario['eyr'] in range(2020,2031)):
-    #        print('byr, iyr and eyr ok')
-
-
-
-    #if ('byr' in linha and 'iyr' in linha and 'eyr' in linha and 'hgt' in linha and 'hcl' in linha and 'ecl' in linha and 'pid' in linha):
-    #    contador_validos += 1
-    #    print(linha, contador_validos)
-
-
